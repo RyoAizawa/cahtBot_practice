e72b60fea3352b1cf9d6d043b97aa2dfcb45071f
RyoAizawa <aizawaryou6419@gmail.com>
2023-06-20 16:17:09 +0900

    うまくいっていない

--- js/main.js
+++ js/main.js
@@ -9,29 +9,52 @@ import TalkMe from "./components/TalkMe.js";
 // 計算方式の選択肢を出力するコンポーネント
 import SelectCalc from "./components/SelectCalc.js";
 
+const ChatMaster = {
+  template: `
+  <div id="chat">
+  </div>`
+}
+
 Vue.createApp({
   data: () => {
     return {
-      count: 0,       // トリガー
       selected: "",   // 選択した計算方式
       isHide: false,  // 要素を隠す
     };
   },
+  updated() {
+    this.main();
+
+  },
   methods: {
-    // カウンタ（トリガー）を加算するメソッド
-    countUp() {this.count++;},
+    async main() {
+      try {
+        console.log(TalkAdviser);
+        let chat = document.getElementById("chat");
+        chat.insertAdjacentHTML("beforeend", `<talk-adviser :icon="true"> ２つの方法で相場を計算することができます。</talk-adviser>`)
+        await this.wait(2);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    wait(sec) {
+      return new Promise((resolve, reject) => {
+        setTimeout(resolve, sec*1000);
+        //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
+      });
+    },
     // 計算方式を選んだ際に呼ばれるメソッド
     selectedBtn(selectedWord) {
       this.selected = selectedWord;
       this.isHide = true;
-      this.countUp()
     },
   },
   components: {
-    Loading,
-    TalkAdviser,
-    TalkAdviserImg,
-    TalkMe,
-    SelectCalc,
+    "loading" : Loading,
+    "talk-adviser" : TalkAdviser,
+    "talk-adviser-img" : TalkAdviserImg,
+    "talk-me" : TalkMe,
+    "select-calc" : SelectCalc,
+    "chat-master" : ChatMaster,
   },
 }).mount("#app");
